b8f355bea50b1eecc19ecf514c93a06bb3a30ea7
Mohit Gadhavi <mohit.gadhavi@zybisys.com>
2022-08-17 14:26:52 +0530

    reg

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,14 +6,15 @@ import AuthSliceReducer from "./slice/AuthSlice";
 export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: {
-        // Ignore these action types
-        ignoredActions: ["your/action/type"],
-        // Ignore these field paths in all actions
-        ignoredActionPaths: ["meta.arg", "payload.timestamp"],
-        // Ignore these paths in the state
-        ignoredPaths: ["items.dates"],
-      },
+      serializableCheck: false,
+      // {
+      //   // Ignore these action types
+      //   ignoredActions: ["your/action/type"],
+      //   // Ignore these field paths in all actions
+      //   ignoredActionPaths: ["meta.arg", "payload.timestamp"],
+      //   // Ignore these paths in the state
+      //   ignoredPaths: ["items.dates"],
+      // },
     }),
   reducer: {
     FetchData: FetchDataReducer,
